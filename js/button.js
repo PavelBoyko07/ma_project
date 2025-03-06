18626d0f1ea3b5639821d415710ae4a67a0e2c32
Павел Бойко <boyko2023@spobip-bel.ru>
2025-03-06 15:13:05 +0300

    заливаем проект 6 раз

---
+++ js/button.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-btn');
+const headerMenu = document.querySelector('.header__mobile');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
+  headerMenu.classList.toggle('active');
+});
+
+
